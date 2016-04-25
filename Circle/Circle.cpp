e161a26c5fb2d9a3f1b2435d28ccb9cb8a951784
Maksym Bondar <bondarmaksymwork@gmail.com>
2016-04-25 23:02:43 +0300

    Files with code

---
+++ Circle/Circle.cpp
@@ -0,0 +1,89 @@
+// Circle.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <Windows.h>
+#include <time.h>
+#include <math.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <conio.h>
+
+#define width 1000
+#define height 1000
+bool flag = false;
+
+void DrawField(float x0, float y0){
+	glColor3f(1.0, 1.0, 0.0);
+	glPointSize(4);
+	//glPushMatrix();
+	//for (int j = 0; j < 8; ++j){
+		//glRotatef(-45 * j, 0, 0, 1);
+	glBegin(GL_POINTS);
+	int num_segments = 5000;
+	for (int i = 0; i < num_segments; ++i){
+		float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle 
+		float x = 2 * cosf(theta);//calculate the x component 
+		float y = 2 * sinf(theta);//calculate the y component 
+		glVertex2f(x + x0, y + y0);//output vertex 
+	}
+	glEnd();
+		//glPopMatrix();
+	//}
+}
+
+void Tick(float k = 0){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	for (int j = 0; j < 8; ++j){
+		DrawField(k, 0);
+		glRotatef(-45 * (j+1), 0, 0, 1);
+	}
+	glPopMatrix();
+	glFlush();
+	Sleep(100);
+}
+
+void display()
+{
+	glClearColor(0, 0, 0, 0); // цвет фона
+	glClear(GL_COLOR_BUFFER_BIT); // очистка буфера цвета
+	glColor3f(1, 1, 1);
+	glFlush();
+}
+
+void timer(int = 0){
+	Sleep(1000);
+	if (!flag){
+		for (float i = 0.f; i <= 1.65f; i += 0.2f){
+			Tick(i);
+		}
+	}
+	else{
+		for (float i = 1.65f; i >= 0.f; i -= 0.2f){
+			Tick(i);
+		}
+	}
+	flag ? flag = false : flag = true;
+	glutTimerFunc(50, timer, 0);
+}
+
+int main(int argc, char **argv)
+{
+	srand((unsigned)time(NULL));//randomize random
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Circle");
+	glClearColor(0.0, 0.0, 0.0, 0.0);//set bachground's color 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-5, 5, -5, 5);
+
+	glutDisplayFunc(display);
+	glutTimerFunc(50, timer, 0);
+
+	glutMainLoop();
+	return 0;
+}
+
